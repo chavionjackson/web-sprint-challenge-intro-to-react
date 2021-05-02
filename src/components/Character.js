fdb6205e28fc69e97ea810975440e3821a7f1c4f
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-05-01 23:36:23 -0700

    Done

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,8 +2,15 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const StyleNames = styled.div`
-    color: white;
+const Card = styled.div`
+    border: tan 5px solid;
+    font-size: 1.3vw;
+    display:flex;
+    justify-content: space-around;
+`
+
+const Space = styled.div`
+    padding: 2vw;
 `
 
 const StarWars = (props) => {
@@ -11,11 +18,15 @@ const StarWars = (props) => {
 
     return (
         <div>
+            <Space>
             <h1>{data.name}</h1>
+            <Card>
             <p>Height: {data.height}</p>
             <p>Mass: {data.mass}</p>
             <p>Hair Color: {data.hair_color}</p>
             <p>Skin Color: {data.skin_color}</p>
+            </Card>
+            </Space>
         </div>
     )
 }
